77645d81f0926e496723a312630933b2d38b1b93
bohemio <adderchavajay7@gmail.com>
2024-03-27 15:37:36 -0600

    instalacion y prubas con tailwind css

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('template')
+
+@section('content')
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <div class="bg-sky-400 text-white text-2xl">
+      otras Secciones
+    </div>
+@endsection
