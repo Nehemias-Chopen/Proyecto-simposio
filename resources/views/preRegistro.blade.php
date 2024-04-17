1078e41d8adf9e3bb0f447d083d883e56dbf8be5
bohemio <adderchavajay7@gmail.com>
2024-04-17 00:11:21 -0600

    vista preRegistro respondivo

--- resources/views/preRegistro.blade.php
+++ resources/views/preRegistro.blade.php
@@ -2,32 +2,32 @@
 
 @section('content')
 <div class="m-auto">
-  <div class="w-[50rem] flex flex-col gap-10 px-4 py-6 md:p-10 lg:p-12 shadow-md rounded-xl bg-white m-10 lg:m-16">
+  <div class="w-[20rem] lg:w-[50rem] flex flex-col gap-10 px-4 py-6 md:p-10 lg:p-12 shadow-md rounded-xl bg-white m-10 lg:m-16">
     <h1 class="text-xl lg:text-2xl font-semibold text-center"> Seleciona algun tipo de gestion</h1>
     <div class="flex flex-col gap-4">
       <div class="flex">
-        <p class="w-20 flex items-center">Nombre</p>
-        <input type="text" placeholder="Nombre" class="placeholder:text-sm focus:outline-none bg-slate-200 py-2 px-3 rounded-full">
+        <p class="hidden lg:w-20 lg:flex lg:items-center">Nombre</p>
+        <input type="text" placeholder="Nombre" class="w-full lg:w-60 placeholder:text-sm focus:outline-none bg-slate-200 py-2 px-3 rounded-full">
       </div>
       <div class="flex">
-        <p class="w-20 flex items-center">Carne</p>
-        <input type="text" placeholder="Carne" class="placeholder:text-sm focus:outline-none bg-slate-200 py-2 px-3 rounded-full">
+        <p class="hidden lg:w-20 lg:flex lg:items-center">Carne</p>
+        <input type="text" placeholder="Carne" class="w-full lg:w-60 placeholder:text-sm focus:outline-none bg-slate-200 py-2 px-3 rounded-full">
+      </div>
+      <div class=" relative lg:flex">
+        <p class="hidden lg:w-20 lg:flex lg:items-center">Semestre</p>
+        <input type="text" placeholder="Semestre" class="w-full lg:w-60 placeholder:hidden lg:placeholder:flex lg:placeholder:text-sm lg:focus:outline-none bg-slate-200 py-2 px-3 rounded-full">
       </div>
       <div class="flex">
-        <p class="w-20 flex items-center">Semestre</p>
-        <input type="text" placeholder="Semestre" class="placeholder:text-sm focus:outline-none bg-slate-200 py-2 px-3 rounded-full">
-      </div>
-      <div class="flex">
-        <p class="w-20 flex items-center">Numero</p>
-        <input type="text" placeholder="Numero" class="placeholder:text-sm focus:outline-none bg-slate-200 py-2 px-3 rounded-full">
+        <p class="hidden lg:w-20 lg:flex lg:items-center">Numero</p>
+        <input type="text" placeholder="Numero" class="w-full lg:w-60 placeholder:text-sm focus:outline-none bg-slate-200 py-2 px-3 rounded-full">
       </div>
 
-      <div class="flex py-5 gap-10">
-        <p class="w-20 flex items-center">Inscripcion</p>
-        <input type="text" placeholder="Inscripcion" class="placeholder:text-sm focus:outline-none bg-slate-200 py-2 px-3 rounded-full">
+      <div class="flex lg:py-5 gap-5 lg:gap-10">
+        <p class="hidden lg:w-20 lg:flex lg:items-center">Inscripcion</p>
+        <input type="text" placeholder="Inscripcion" class="w-full lg:w-60 placeholder:text-sm focus:outline-none bg-slate-200 py-2 px-3 rounded-full">
       </div>
 
-      <div class="flex gap-10">
+      <div class="flex flex-col lg:flex-row gap-5 lg:gap-10">
         <span class="">Seleccione la talla de la playera</span>
         <select name="" id="" class="py-2 px-3 rounded-full bg-slate-200 w-20 text-sm font-bold">
           <option value="">S</option>
@@ -36,7 +36,7 @@
         </select>
       </div>
       
-      <div class="flex gap-5 justify-between items-end">
+      <div class="flex w-full flex-col lg:flex-row gap-5 lg:flex lg:justify-between lg:items-end">
         <div>
           <h3 class="font-bold text-lg">Añade suavenirs</h3>
           
@@ -56,7 +56,8 @@
             <span>+Q.80.00</span>
           </div>
         </div>
-        <div class="flex gap-4">
+
+        <div class="flex gap-7 lg:gap-4">
           <span class="font-bold">
             subtotal
           </span>
@@ -68,7 +69,7 @@
         </div>
       </div>
 
-      <div class="pt-5 flex justify-end gap-5">
+      <div class="pt-5 flex justify-center lg:justify-end gap-5">
         <button class="border border-slate-800 py-2 px-3 rounded-full text-sm font-bold active:bg-slate-800 hover:bg-slate-500 hover:text-white">anterior</button>
         <button class="py-2 px-3 rounded-full bg-sky-900 text-white text-sm font-bold active:bg-sky-950 hover:bg-sky-700 hover:text-white">siguiente</button>
       </div>
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -7,6 +7,9 @@
   
 </head>
 <body class="min-h-[100vh] flex justify-center items-center flex-col ">
+  <header class="bg-sky-900 w-full py-2 shadow-2xl sticky top-0">
+    <img src="https://www.logolynx.com/images/logolynx/46/462b25e81d9022b037b0f07b4705bff0.png" alt="Logo Mariano Galvez" class="m-auto w-20 h-16">
+  </header>
   @yield('content')
   <footer class="w-full">
     <img src="{{ asset('/img/Fogata.svg') }}" alt="Imagen Fogata" class="w-full object-cover">
